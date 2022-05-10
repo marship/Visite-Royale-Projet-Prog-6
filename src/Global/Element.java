99e867a43ef3a7230a6ae43814d21bd4b5bb38cb
Aglemba <94596458+Aglemba@users.noreply.github.com>
2022-05-10 12:50:10 +0200

    Version fonctionnel énumération
    - Mise à jour de l'énumération

---
+++ src/Global/Element.java
@@ -0,0 +1,22 @@
+package Global;
+
+public enum Element {
+
+    VIDE (-1),
+    COURONNE (0),
+    GARDE_GAUCHE (1),
+    GARDE_DROIT (2),
+    ROI (3),
+    FOU (4),
+    SORCIER (5);
+
+    int valeurElement;
+
+    Element(int valeurElement) {
+        this.valeurElement = valeurElement;
+    }
+
+    public int getValeurElement() {
+        return valeurElement;
+    }
+}
