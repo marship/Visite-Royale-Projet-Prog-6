44735dfbe397cd2fae0b8687daea4122f4de1bd3
Aglemba <94596458+Aglemba@users.noreply.github.com>
2022-05-19 13:55:21 +0200

    Nettoyage AdapteurSouris
    - Factorisation méthode

--- src/Vue/AdaptateurSouris.java
+++ src/Vue/AdaptateurSouris.java
@@ -3,18 +3,33 @@ package Vue;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import Global.Configuration;
+
 public class AdaptateurSouris extends MouseAdapter {
 
     static final int MILIEU_PLATEAU = 8;
 
+    static final int AUCUNE_OPTION = 0;
+    static final int COORDONNEE_PLATEAU_X = 1;
+    static final int COORDONNEE_PLATEAU_Y = 2;
+    static final int COORDONNEE_MAIN_X = 3;
+
     PlateauGraphique plateauGraphique;
     CollecteurEvenements collecteurEvenements;
 
+    /////////////////////////////////////////////////////////////////////////
+
+    // ========================
+    // ===== CONSTRUCTEUR =====
+    // ========================
     AdaptateurSouris(PlateauGraphique pGraphique, CollecteurEvenements cEvenements) {
         plateauGraphique = pGraphique;
         collecteurEvenements = cEvenements;
     }
 
+    // =======================
+    // ===== CLIC SOURIS =====
+    // =======================
     @Override
     public void mousePressed(MouseEvent e) {
 
@@ -22,17 +37,13 @@ public class AdaptateurSouris extends MouseAdapter {
         int clicY = e.getY();
 
         if (estClicSurPlateau(clicX, clicY)) {
-
-            clicX = conversionCoordonnee(clicX, true);
-            clicY = conversionCoordonnee(clicY, false);
-
+            clicX = conversionCoordonnee(clicX, COORDONNEE_PLATEAU_X);
+            clicY = conversionCoordonnee(clicY, COORDONNEE_PLATEAU_Y);
             collecteurEvenements.clicPlateau(clicX, clicY);
-
-        } else {
-            if (clicX >= plateauGraphique.debutZoneCartesX() && clicX <= plateauGraphique.finZoneCartesX() && clicY >= plateauGraphique.debutZoneCartesY() && clicY <= plateauGraphique.finZoneCartesY()) {
-                clicX = e.getX() / plateauGraphique.largeurCarte() - 4;
-                collecteurEvenements.clicCarte(clicX);
-            }
+            
+        } else if (estClicSurMain(clicX, clicY)) {
+            clicX = conversionCoordonnee(clicX, COORDONNEE_MAIN_X);
+            collecteurEvenements.clicCarte(clicX);
         }
     }
 
@@ -40,11 +51,21 @@ public class AdaptateurSouris extends MouseAdapter {
         return (((clicX >= plateauGraphique.debutPlateauX()) && (clicX <= plateauGraphique.largeurPlateau())) && ((clicY >= plateauGraphique.debutPlateauY()) && (clicY <= plateauGraphique.hauteurPlateau())));
     }
 
-    int conversionCoordonnee(int clic, Boolean estCoordonneeX) {
-        if (estCoordonneeX) {
-            return (clic / plateauGraphique.largeurCasePlateau() - MILIEU_PLATEAU);
-        } else {
-            return (clic / plateauGraphique.quartHauteurPlateau() - 1);
+    boolean estClicSurMain(int clicX, int clicY) {
+        return (((clicX >= plateauGraphique.debutZoneCartesX()) && (clicX <= plateauGraphique.finZoneCartesX())) && ((clicY >= plateauGraphique.debutZoneCartesY()) && (clicY <= plateauGraphique.finZoneCartesY())));
+    }
+
+    int conversionCoordonnee(int clic, int option) {
+        switch (option) {
+            case COORDONNEE_PLATEAU_X:
+                return (clic / plateauGraphique.largeurCasePlateau() - MILIEU_PLATEAU);
+            case COORDONNEE_PLATEAU_Y:
+                return (clic / plateauGraphique.largeurCarte() - 4);
+            case COORDONNEE_MAIN_X:
+                return (clic / plateauGraphique.quartHauteurPlateau() - 1);       
+            default:
+                Configuration.instance().logger().warning("Option Invalide !!");
+                return AUCUNE_OPTION;
         }
     }
 }
